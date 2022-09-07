670f5d6907d209a36a8086e4834af8baae21ac49
Mónika <gergye.monika@gmail.com>
2022-09-07 15:54:03 +0200

    első házi kész

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,20 @@
+
+function isString(srt){
+    return typeof srt !== 'string'
+}
+
+
+
+const convertToUppercase = (arr=[a,b])=>{
+    return new Promise ((resolved, rejected)=>{
+        //if (!arr.some(string=>typeof string !== 'string')){
+        if (!arr.some(isString)){
+            const transformedArr = arr.map(item=>item.toUpperCase());
+            resolved (transformedArr)
+        } else {
+            rejected (new Error('Error: Not all items in the array are strings!'))
+        }
+    })
+}
+
 export default convertToUppercase;
